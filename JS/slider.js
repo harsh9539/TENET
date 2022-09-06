a13f06910b0b38f2d2b73f1f65b40cdca0728f9c
harsh9539 <goyalh862@gmail.com>
2022-09-07 03:20:49 +0530

    carousel added pic left

--- JS/slider.js
+++ JS/slider.js
@@ -88,3 +88,20 @@ function playCarousel(nextButton, config) {
 }
 
 start();
+
+
+
+// past events
+
+function rotate() {
+    var lastChild = $('.slider div:last-child').clone();
+    /$('#test').html(lastChild)/
+    $('.slider div').removeClass('firstSlide')
+    $('.slider div:last-child').remove();
+    $('.slider').prepend(lastChild)
+    $(lastChild).addClass('firstSlide')
+  }
+  
+  window.setInterval(function(){
+    rotate()
+  }, 2000);
